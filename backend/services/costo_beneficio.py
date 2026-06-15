0fe93e67bbeb12c74baaacdc2b97b5cae4b34b32
unknown <VMILLA@JVALDIVIAC-PC.imovalle.local>
2026-06-15 15:56:32 -0400

    Cambios formula costo-beneficio

--- backend/services/costo_beneficio.py
+++ backend/services/costo_beneficio.py
@@ -9,22 +9,35 @@ def calcular_costo(df_costo):
             errors="coerce"
         ).fillna(0)
 
-    # Total búsquedas
+    # TOTAL BUSQUEDA, NO SCAN
     df_costo["BUSQUEDAS"] = (
         df_costo["USER_SEEKS"]
-        + df_costo["USER_SCANS"]
         + df_costo["USER_LOOKUPS"]
     )
 
     def clasificar(row):
         busquedas = row["BUSQUEDAS"]
         updates = row["USER_UPDATES"]
+        ratio = updates/ busquedas
 
         if busquedas == 0 and updates == 0:
             return "NO USO"
 
-        if busquedas < updates:
-            return "MAYOR COSTO DE MANTENCION"
+        if ratio <= 1:
+            return "Beneficio Busqueda"
+        
+        if 1 <ratio <1.5:
+            return "Aceptable"
+        
+        if 1.5 <ratio <3:
+            return "Costo de mantenimiento"
+        
+        if ratio < 3:
+            return "Relevante costo de mantenimiento"
+        
+        if ratio < 10:
+            return "Trascendental costo de mantenimiento"
+
 
         if busquedas >= updates:
             return "BENEFICIO EN BUSQUEDA"
@@ -33,6 +46,5 @@ def calcular_costo(df_costo):
 
     df_costo["EVALUACION_INDICE"] = df_costo.apply(clasificar, axis=1)
 
-    #df_costo.to_csv("costo-benefico.csv",sep=";",index=False)
-    #print("Archivo generado: indices_evaluados.csv")
+    df_costo.to_csv("costo-benefico.csv",sep=";",index=False)
     return df_costo
