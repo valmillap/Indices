36f3a2b6934be96173687cd6c5b4edd72f78a0dc
valmillap <valmillap@gmail.com>
2026-07-02 11:22:22 -0400

    Heap tabla

--- frontend/src/config/lookup.jsx
+++ frontend/src/config/lookup.jsx
@@ -2,8 +2,9 @@
 
   export const lookupColumnDefs = [
     {field: "TABLA", flex: 1.1,
+        
         cellRenderer: params => {
-        if (params.data.TYPE_DESC !== "HEAP") return ""
+        if (params.data.TYPE_DESC !== "HEAP") return "Posible causante lookup"
         return params.value
     },
         cellClassRules: {
@@ -11,10 +12,12 @@
                 String(params.data.TYPE_DESC).trim() == "HEAP",
             "celda-borde": params =>
                 String(params.data.TYPE_DESC).trim() == "HEAP",
+             "celda-italic": params =>
+                String(params.data.TYPE_DESC).trim() == "NONCLUSTERED",
         }
     },
     {field: "INDICE", flex: 1},
-    {field: "TYPE_DESC", flex: 1,
+    {field: "TYPE_DESC", flex: 0.7,
         cellRenderer: params => {
         if (params.value === "HEAP") {
             return <span className="pill-heap">{params.value}</span>;
@@ -24,18 +27,33 @@
             }
         }
     },
-    {field: "USER_LOOKUPS",flex: 1,
+    {headerName:"LOOKUPS",field: "USER_LOOKUPS",flex: 0.5,
        cellClassRules: {
             "celda-lookup": params =>
                 Number(params.value) > 1000,
+        },
+        cellRenderer: params => {
+        if (params.data["USER_LOOKUPS"] == 0) return ""
+        return params.value
         }
     },
-    {field: "USER_SEEKS", flex: 1},
-    {field: "USER_SCANS", flex: 1},
-    {field: "USED-PAGES", flex: 1}
+    {headerName:"SEEKS",field: "USER_SEEKS", flex: 0.4},
+    {headerName:"SCANS",field: "USER_SCANS",flex: 0.4},
+    {field: "USED-PAGES", flex: 1},
+    {headerName: "COLUMNA",flex:1,
+    valueGetter: params => getColumna(params.data["TYPE_DESC"]),
+    cellClassRules: {
+            "celda-italic": params =>
+                String(params.data.TYPE_DESC).trim() == "NONCLUSTERED",
+        },
+    },
     
 ];
 
+const getColumna = (type) => {
+    if (type.trim() =="NONCLUSTERED")  return "algo"
+    return " "
+}
 
 export const lookupRules = {
     "fila-heap": params =>
